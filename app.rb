cd96f83a83f4b095545a0eeaab0dfbfd089a7a3b
David Ritchey <ritchey0713@gmail.com>
2018-05-14 01:06:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
      end 
      
      get "/say/:number/:phrase" do 
-       @phrases= <%= "#{:number}.do 
+       <%= "#{:number}.do 
          #{:phrase}"%>
        end 
 
