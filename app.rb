d0da15ba55355ac24cf80a7b8fcd43526238b150
David Ritchey <ritchey0713@gmail.com>
2018-05-14 01:21:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,7 +26,12 @@ class App < Sinatra::Base
      end 
      
      get "/:operation/:number1/:number2" do 
-       "#{params[:number1]} #{params[:operation]} #{:number2}"
+       @operation = params[:operation]
+       @number1 = params[:number1]
+       @number2 = params[:number2]
+       
+       if @operation == "add"
+         @number1 + 
        
      end 
     
